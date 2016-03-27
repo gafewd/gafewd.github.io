1a1ccd85da7ed309463f5a1f57ba6e5534560c25
Chandler Moisen <chandler@percolate.com>
2016-03-26 20:31:41 -0400

    update class 8 notes

--- materials/class08/class08/rgb-color-choice-solution/js/project.js
+++ materials/class08/class08/rgb-color-choice-solution/js/project.js
@@ -13,3 +13,18 @@
 // 5. Use .innerHTML to change the text inside of #colorful-text to colorStr
 
 // 6. Use .style.background to change the background of #wrapper to colorStr
+
+
+document.getElementById('color-button').onclick = changeColor
+
+function changeColor () {
+	var red = document.getElementById('red').value;
+	var green = document.getElementById('green').value;
+	var blue = document.getElementById('blue').value;
+
+	var colorStr = "rgb(" + red + "," + green + "," + blue + ")";
+
+	document.getElementById("colorful-text").innerHTML = colorStr;
+
+	document.getElementById('wrapper').style.background = colorStr;
+}
